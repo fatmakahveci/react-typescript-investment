2d1008c4812cd4e933479c4ff5f2ed03cb30e41e
fatmakhv <fatmaba@gmail.com>
2023-08-29 13:43:39 +0300

    Methods are updated.

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -1,41 +1,50 @@
 "use client";
 
+import { FormEvent } from "react";
+
 const Form: () => JSX.Element = () => {
+    const submitHandler = (e: FormEvent) => {
+        e.preventDefault(); // avoid page reloading
+        // TODO: Complete
+    };
+
+    const resetHandler = () => {
+        // TODO: Complete
+    };
+
     return (
-        <div>
-            <form className="form">
-                <div className="input-group">
-                    <p>
-                        <label htmlFor="current-savings">Current Savings ($)</label>
-                        <input type="number" id="current-savings" />
-                    </p>
-                    <p>
-                        <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-                        <input type="number" id="yearly-contribution" />
-                    </p>
-                </div>
-                <div className="input-group">
-                    <p>
-                        <label htmlFor="expected-return">
-                            Expected Interest (%, per year)
-                        </label>
-                        <input type="number" id="expected-return" />
-                    </p>
-                    <p>
-                        <label htmlFor="duration">Investment Duration (years)</label>
-                        <input type="number" id="duration" />
-                    </p>
-                </div>
-                <p className="actions">
-                    <button type="reset" className="buttonAlt">
-                        Reset
-                    </button>
-                    <button type="submit" className="button">
-                        Calculate
-                    </button>
+        <form onSubmit={submitHandler} className="form">
+            <div className="input-group">
+                <p>
+                    <label htmlFor="current-savings">Current Savings ($)</label>
+                    <input type="number" id="current-savings" />
                 </p>
-            </form>
-        </div>
+                <p>
+                    <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+                    <input type="number" id="yearly-contribution" />
+                </p>
+            </div>
+            <div className="input-group">
+                <p>
+                    <label htmlFor="expected-return">
+                        Expected Interest (%, per year)
+                    </label>
+                    <input type="number" id="expected-return" />
+                </p>
+                <p>
+                    <label htmlFor="duration">Investment Duration (years)</label>
+                    <input type="number" id="duration" />
+                </p>
+            </div>
+            <p className="actions">
+                <button onClick={resetHandler} type="reset" className="buttonAlt">
+                    Reset
+                </button>
+                <button type="submit" className="button">
+                    Calculate
+                </button>
+            </p>
+        </form>
     )
 }
 
