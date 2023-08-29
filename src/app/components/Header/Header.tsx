bc36f8abff565ac3103a3105392060d2b20b3cd5
fatmakhv <fatmaba@gmail.com>
2023-08-29 13:41:31 +0300

    <div> is fixed.

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -5,12 +5,10 @@ import logo from "../../../assets/investment-calculator-logo.png";
 
 const Header: () => JSX.Element = () => {
     return (
-        <div>
-            <header className="header">
-                <Image src={logo} alt="logo" />
-                <h1>Investment Calculator</h1>
-            </header>
-        </div>
+        <header className="header">
+            <Image src={logo} alt="logo" />
+            <h1>Investment Calculator</h1>
+        </header>
     )
 }
 export default Header;
