7b3e2c1474ad3457c9ce4c5ac5a3d5844b44d9c2
fatmakhv <fatmaba@gmail.com>
2023-08-31 18:19:28 +0300

    Constants are added.

--- src/shared/Constants.tsx
+++ src/shared/Constants.tsx
@@ -1,8 +1,8 @@
 import { FormModel } from "@/shared/types/Types";
 
 export const INITIAL_VALUES: FormModel = {
-    "current-savings": 0,
-    "yearly-contribution": 0,
-    "expected-return": 0,
-    "duration": 0,
+    "current-savings": 0.0,
+    "yearly-contribution": 0.0,
+    "expected-return": 0.0,
+    "duration": 0.0,
 };
