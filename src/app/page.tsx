e9cf44647e7b12cd19dfb3aadc6b2d24eb919e18
fatmakhv <fatmaba@gmail.com>
2023-08-31 18:20:50 +0300

    Constants are separated. Calculate Handler is updated. Comments are fixed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,26 @@
 'use client';
 
+import { FormModel, YearlyData } from '@/shared/types/Types';
 import Form from './components/Form/Form';
 import Header from './components/Header/Header';
 import Table from './components/Table/Table';
 import './globals.css';
 
-type YearlyData = {
-  year: number,
-  yearlyInterest: number,
-  savingsEndOfYear: number,
-  yearlyContribution: number
-};
-
 const Home = ({ }): JSX.Element => {
-  const calculateHandler: any = (userInput: any): void => {
-    // TODO: Should be triggered when form is submitted
-    // TODO: You might not directly want to bind it to the submit event on the form though...
+  const calculateHandler = (formData: FormModel): void => {
+    let yearlyData: YearlyData[] = []; // per-year results
 
-    const yearlyData: YearlyData[] = []; // per-year results
-
-    // TODO: Change the shape of input object
-    let currentSavings: number = +userInput["current-savings"];
-
-    // TODO: Change the shape of input object
-    const yearlyContribution: number = +userInput["yearly-contribution"];
-
-    const expectedReturn: number = +userInput["expected-return"] / 100;
-    const duration: number = +userInput["duration"];
+    let currentSavings: number = +formData["current-savings"];
+    let yearlyContribution: number = +formData["yearly-contribution"];
+    let expectedReturn: number = +formData["expected-return"] / 100;
+    let duration: number = +formData["duration"];
 
     // It calculates yearly results (total savings, interest etc)
     for (let i: number = 0; i < duration; i++) {
       const yearlyInterest: number = currentSavings * expectedReturn;
       currentSavings += yearlyInterest + yearlyContribution;
 
-      // TODO: Change the shape of pushed object
-      yearlyData.push({
+      yearlyData.push({ // TODO: Change the shape of pushed object
         year: i + 1,
         yearlyInterest: yearlyInterest,
         savingsEndOfYear: currentSavings,
@@ -48,7 +34,7 @@ const Home = ({ }): JSX.Element => {
   return (
     <>
       <Header />
-      <Form />
+      <Form onCalculate={calculateHandler}/>
       <Table />
     </>
   );
