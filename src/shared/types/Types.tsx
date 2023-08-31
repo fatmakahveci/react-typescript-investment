9df9271176455f12e6082c744dc7087594180dad
fatmakhv <fatmaba@gmail.com>
2023-08-31 23:06:28 +0300

    prev is added.

--- src/shared/types/Types.tsx
+++ src/shared/types/Types.tsx
@@ -2,12 +2,13 @@ export type FormModel = {
     "current-savings": number,
     "yearly-contribution": number,
     "expected-return": number,
-    "duration": number
+    "duration": number,
+    "prevState"?: null
 };
 
 export type YearlyData = {
-    year: number,
-    yearlyInterest: number,
-    savingsEndOfYear: number,
-    yearlyContribution: number
+    "year": number,
+    "yearlyInterest": number,
+    "savingsEndOfYear": number,
+    "yearlyContribution": number
 };
