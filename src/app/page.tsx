86ca7af0bf264ffea26ff0e87041b1da0a543553
fatmakhv <fatmaba@gmail.com>
2023-08-31 23:08:30 +0300

    Code cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,40 +4,49 @@ import { FormModel, YearlyData } from '@/shared/types/Types';
 import Form from './components/Form/Form';
 import Header from './components/Header/Header';
 import Table from './components/Table/Table';
+import { useState } from 'react';
 import './globals.css';
 
 const Home = ({ }): JSX.Element => {
-  const calculateHandler = (formData: FormModel): void => {
-    let yearlyData: YearlyData[] = []; // per-year results
+  const [formData, setFormData] = useState<FormModel | null>(null);
 
+  const calculateHandler = (formData: FormModel): void => {
+    setFormData(formData);
+  };
+
+  let yearlyData: YearlyData[] = []; // per-year results
+
+  if (formData) {
     let currentSavings: number = +formData["current-savings"];
-    let yearlyContribution: number = +formData["yearly-contribution"];
-    let expectedReturn: number = +formData["expected-return"] / 100;
-    let duration: number = +formData["duration"];
+    const yearlyContribution: number = +formData["yearly-contribution"];
+    const expectedReturn: number = +formData["expected-return"] / 100;
+    const duration: number = +formData["duration"];
 
     // It calculates yearly results (total savings, interest etc)
     for (let i: number = 0; i < duration; i++) {
       const yearlyInterest: number = currentSavings * expectedReturn;
       currentSavings += yearlyInterest + yearlyContribution;
 
-      yearlyData.push({ // TODO: Change the shape of pushed object
+      yearlyData.push({
+        // TODO: Change the shape of pushed object
         year: i + 1,
         yearlyInterest: yearlyInterest,
         savingsEndOfYear: currentSavings,
-        yearlyContribution: yearlyContribution
+        yearlyContribution: yearlyContribution,
       });
     }
 
     // TODO: do something with yearlyData ...
-  };
+  }
 
   return (
     <>
       <Header />
-      <Form onCalculate={calculateHandler}/>
-      <Table />
+      <Form onCalculate={calculateHandler} />
+      {!formData && <p>No investment calculated yet!</p>}
+      {formData && <Table data={yearlyData} />}
     </>
   );
-}
+};
 
 export default Home;
