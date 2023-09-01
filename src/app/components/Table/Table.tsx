682747317df64bbbacba84f43afcc275d8112ead
fatmakhv <fatmaba@gmail.com>
2023-09-01 12:02:52 +0300

    Data is added. Data format is changed.

--- src/app/components/Table/Table.tsx
+++ src/app/components/Table/Table.tsx
@@ -6,9 +6,17 @@ import { FC } from 'react';
 
 interface Props {
     data: YearlyData[];
+    initialInvestment: number;
 };
 
-const Table: FC<Props> = ({ data }): JSX.Element => {
+const formatter: Intl.NumberFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+});
+
+const Table: FC<Props> = ({ data, initialInvestment }): JSX.Element => {
     return (
         <table className="result">
             <thead>
@@ -21,13 +29,15 @@ const Table: FC<Props> = ({ data }): JSX.Element => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>YEAR NUMBER</td>
-                    <td>TOTAL SAVINGS END OF YEAR</td>
-                    <td>INTEREST GAINED IN YEAR</td>
-                    <td>TOTAL INTEREST GAINED</td>
-                    <td>TOTAL INVESTED CAPITAL</td>
-                </tr>
+                {data.map((yearData) => (
+                    <tr key={yearData.year}>
+                        <td>{yearData.year}</td>
+                        <td>{formatter.format(yearData.savingsEndOfYear)}</td>
+                        <td>{formatter.format(yearData.yearlyInterest)}</td>
+                        <td>{formatter.format(yearData.savingsEndOfYear - initialInvestment - yearData.yearlyContribution * yearData.year)}</td>
+                        <td>{formatter.format(initialInvestment + yearData.yearlyContribution * yearData.year)}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     )
